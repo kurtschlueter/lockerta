da07d36b35abc6e676fff654cc833f4f32ec2fe0
Kurt Schlueter <kurtbschlueter@gmail.com>
2016-06-03 14:42:16 -0500

    added delays for keyup events

--- app/assets/javascripts/search_autocomplete.js
+++ app/assets/javascripts/search_autocomplete.js
@@ -1,28 +1,77 @@
 var autoComplete = function() {
+var delay_in_ms = 500;
+
+
+  var delay = (function(){
+    var timer = 0;
+    return function(callback, ms){
+      clearTimeout (timer);
+      timer = setTimeout(callback, ms);
+    };
+  })();
 
   // console.log('entered doc ready')
-  $(".school_search").keyup(function(){
+  $(".school_search").keyup(function(e){
     // console.log('entered keyup')
 
-    var input = $(".school_search").val()
-    $(".schools_table").empty()
 
-    if (input == "") {
-        $('.schools_table').addClass('hidden')
+// OK I know this isn't dry but I have to move on for now. If the keyup is a backspace, then I want the delay to be zero. If it is anything else, then It should wait 500 ms.
+
+    var key_hit = e.keyCode;
+
+
+    if (key_hit == 8) {
+
+      delay(function(key_hit){
+        var input = $(".school_search").val()
+        $(".schools_table").empty()
+
+        //If input is empty, hide table.
+        if (input == "") {
+            $('.schools_table').addClass('hidden')
+        } else {
+            $('.schools_table').removeClass('hidden')
+        }
+        $.ajax({
+          url: "/schools/search", // Route to the Script Controller method
+          type: "GET",
+          dataType: "json",
+          data: { input: input },
+          success: function(data) {
+            for (i = 0; i < data.data.length; i++) {
+              $(".schools_table").append("<tr class='danger'><td>"+ data.data[i].name+"</td></tr>");
+            }
+          }
+        });
+      }, 0);
     } else {
-        $('.schools_table').removeClass('hidden')
+
+
+      delay(function(key_hit){
+        var input = $(".school_search").val()
+        $(".schools_table").empty()
+
+        //If input is empty, hide table.
+        if (input == "") {
+            $('.schools_table').addClass('hidden')
+        } else {
+            $('.schools_table').removeClass('hidden')
+        }
+        $.ajax({
+          url: "/schools/search", // Route to the Script Controller method
+          type: "GET",
+          dataType: "json",
+          data: { input: input },
+          success: function(data) {
+            for (i = 0; i < data.data.length; i++) {
+              $(".schools_table").append("<tr class='danger'><td>"+ data.data[i].name+"</td></tr>");
+            }
+          }
+        });
+      }, 500);
+
     }
 
-    $.ajax({
-      url: "/schools/search", // Route to the Script Controller method
-      type: "GET",
-      dataType: "json",
-      data: { input: input },
-      success: function(data) {
-        for (i = 0; i < data.data.length; i++) {
-          $(".schools_table").append("<tr class='danger'><td>"+ data.data[i].name+"</td></tr>");
-        }
-      }
-    });
+
   });
 };
