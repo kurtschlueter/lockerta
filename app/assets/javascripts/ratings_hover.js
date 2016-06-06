b0c9bade89866e89428c0f949bc9308c6c6f3ca1
Kurt Schlueter <kurtbschlueter@gmail.com>
2016-06-06 16:14:31 -0500

    stars only show full when mouse is over

--- app/assets/javascripts/ratings_hover.js
+++ app/assets/javascripts/ratings_hover.js
@@ -2,22 +2,54 @@ var ratingsHover = function() {
   console.log('ratings hover function entered')
 
 
+  var sourceSwap = function () {
+      var $this = $(this);
+      var newSource = $this.data('alt-src');
+      $this.data('alt-src', $this.attr('src'));
+      $this.attr('src', newSource);
 
-  $(document).on('mouseover', '.rating-stars', function(e) {
-    console.log('mouseover entered')
+      debugger
+  }
 
-    var current_id_tag = $(this).attr('id')
-    var current_id_num = parseInt(current_id_tag.slice(-1))
-    var or_source = $(this).children()[0].src
-    var alt_source = $(this).children().data('alt-src')
+  $('.rating-stars-img').hover(sourceSwap, sourceSwap);
+//   $(document).on('mouseover', '.rating-stars', function(e) {
 
-    $(this).children()[0].src = alt_source
-    // for (1 to <= current_id_num ++) {
+//     console.log('mouseover entered')
 
-    //   change to full star
+//     var current_id_tag = $(this).attr('id')
+//     var current_id_num = parseInt(current_id_tag.slice(-1))
+//     var or_source = $(this).children()[0].src
+//     var alt_source = $(this).children().data('alt-src')
 
-    // }
+//     $(this).children()[0].src = alt_source
+//     // $(this).children().data('alt-src') = or_source
 
-  });
+// debugger
+//     // for (1 to <= current_id_num ++) {
+
+//     //   change to full star
+
+//     // }
+
+//   });
+
+//   $(document).on('mouseleave', '.rating-stars', function(e) {
+
+//     console.log('mouseover leave')
+
+//     var current_id_tag = $(this).attr('id')
+//     var current_id_num = parseInt(current_id_tag.slice(-1))
+//     // var or_source = $(this).children()[0].src
+//     // var alt_source = $(this).children().data('alt-src')
+// debugger
+//     $(this).children()[0].src = or_source
+//     // for (1 to <= current_id_num ++) {
+
+//     //   change to full star
+
+//     // }
+
+//   });
+
 
 }
