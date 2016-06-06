90d62148b40d285d0cd36f93b25b49a5369da73b
ray-curran <rpcurr@gmail.com>
2016-06-06 10:22:48 -0500

    Make images display from the amazon s3 bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_endpoint => 's3-us-west-2.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
