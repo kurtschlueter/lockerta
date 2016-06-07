8ead934de18c77890f63d6e30c8a221ab4e78dc3
Kurt Schlueter <kurtbschlueter@gmail.com>
2016-06-06 19:16:00 -0500

    rating click and hover functionalities now work together

--- app/assets/javascripts/ratings_hover.js
+++ app/assets/javascripts/ratings_hover.js
@@ -4,75 +4,101 @@ var ratingsHover = function() {
 
   var sourceSwap = function () {
       var $this = $(this);
-      var newSource = $this.data('alt-src');
-      $this.data('alt-src', $this.attr('src'));
+      var newSource = $this.data('fullstar');
+      $this.data('fullstar', $this.attr('src'));
       $this.attr('src', newSource);
 
     var current_id_tag = $(this).parent().attr('id')
     var current_id_num = parseInt(current_id_tag.slice(-1))
 
-    // var $this2;
-    // var newSource2;
-    // debugger
     var thisArray = $this.parent().prevAll();
     // debugger
     for (var star_index = 0; star_index < thisArray.length; star_index++){
 
     var $this2 = $(thisArray[star_index]).children();
-    var newSource2 = $this2.data('alt-src');
-    $this2.data('alt-src', $this2.attr('src'));
+    var newSource2 = $this2.data('fullstar');
+    // $this2.data('fullstar', $this2.attr('src'));
     $this2.attr('src', newSource2);
+    }
+  }
 
-    // for (var star_index = 0; star_index < $thisArray.length; star_index++){
-    //   debugger
-    //   var $this2 = $thisArray[star_index].children[0];
-    //   var newSource2 = $this2.data('alt-src');
-    //   $this2.data('alt-src', $this2.attr('src'));
-    //   $this2.attr('src', newSource2);
+  // $('.rating-stars-img').hover(sourceSwap, sourceSwap);
+
+  $(document).on('mouseenter', '.rating-stars-img', function(e) {
+console.log('mouseenter')
+    var $this = $(this);
+    if($this.hasClass('selected') == false) {
+      var newSource = $this.data('fullstar');
+      $this.attr('src', newSource);
+    }
+      var current_id_tag = $(this).parent().attr('id')
+      var current_id_num = parseInt(current_id_tag.slice(-1))
+
+      var thisArray = $this.parent().prevAll();
+      // debugger
+
+      for (var star_index = 0; star_index < thisArray.length; star_index++){
+        var $this2 = $(thisArray[star_index]).children();
+        if($this2.hasClass('selected') == false) {
+          var newSource2 = $this2.data('fullstar');
+          $this2.attr('src', newSource2);
+        }
+      }
+  });
+
+
+  $(document).on('mouseleave', '.rating-stars-img', function(e) {
+console.log('mouseleave')
+    var $this = $(this);
+    if($this.hasClass('selected') == false) {
+      var newSource = $this.data('emptystar');
+      $this.attr('src', newSource);
+    }
+    var current_id_tag = $(this).parent().attr('id')
+    var current_id_num = parseInt(current_id_tag.slice(-1))
+
+    var thisArray = $this.parent().prevAll();
+    // debugger
+    for (var star_index = 0; star_index < thisArray.length; star_index++){
+
+      var $this2 = $(thisArray[star_index]).children();
+      if($this2.hasClass('selected') == false) {
+        var newSource2 = $this2.data('emptystar');
+        $this2.attr('src', newSource2);
+      }
+    }
+
+  });
+
+  $(document).on('click', '.rating-stars', function(e) {
+    $('.rating-stars-img').unbind("mouseleave");
+
+    console.log('entered star click');
+      var $this33 = $(this).children();
+      var newSource33 = $this33.data('fullstar');
+      $this33.data('currentstar', newSource33);
+      $this33.attr('src', newSource33);
+      $this33.addClass('selected');
+
+    var selectedComponents = $this33.parent().prevAll();
+    var deSelectedComponents = $this33.parent().nextAll();
+
+    for (var star_index = 0; star_index < selectedComponents.length; star_index++){
+      $this22 = $(selectedComponents[star_index]).children();
+      $this22.addClass('selected')
+      var newSource2 = $this22.data('fullstar');
+      $this22.attr('src', newSource2);
 
     }
 
-  }
+    for (var unstar_index = 0; unstar_index < deSelectedComponents.length; unstar_index++){
+      var $this232 = $(deSelectedComponents[unstar_index]).children();
+      $this232.removeClass('selected')
+      var newSource22 = $this232.data('emptystar');
+      $this232.attr('src', newSource22);
 
-  $('.rating-stars-img').hover(sourceSwap, sourceSwap);
-//   $(document).on('mouseover', '.rating-stars', function(e) {
-
-//     console.log('mouseover entered')
-
-//     var current_id_tag = $(this).attr('id')
-//     var current_id_num = parseInt(current_id_tag.slice(-1))
-//     var or_source = $(this).children()[0].src
-//     var alt_source = $(this).children().data('alt-src')
-
-//     $(this).children()[0].src = alt_source
-//     // $(this).children().data('alt-src') = or_source
-
-// debugger
-//     // for (1 to <= current_id_num ++) {
-
-//     //   change to full star
-
-//     // }
-
-//   });
-
-//   $(document).on('mouseleave', '.rating-stars', function(e) {
-
-//     console.log('mouseover leave')
-
-//     var current_id_tag = $(this).attr('id')
-//     var current_id_num = parseInt(current_id_tag.slice(-1))
-//     // var or_source = $(this).children()[0].src
-//     // var alt_source = $(this).children().data('alt-src')
-// debugger
-//     $(this).children()[0].src = or_source
-//     // for (1 to <= current_id_num ++) {
-
-//     //   change to full star
-
-//     // }
-
-//   });
+    }
+  });
 
 
 }
