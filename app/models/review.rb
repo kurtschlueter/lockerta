858fd6794071f529f0d8a4e38add707321f88e44
ray-curran <rpcurr@gmail.com>
2016-06-09 14:50:32 -0500

    Add more styling to head coach review display page, add helper to review model for average coach rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ActiveRecord::Base
   belongs_to :school
 
   validates :user_id, :coach_id, :school_id, :program_id, presence: true
+
+  def average_coach_rating
+    ((hc_player_development + hc_player_relationships + hc_recruiting + hc_pro_connections + hc_sport_knowledge)/5).round(1)
+  end
 end
