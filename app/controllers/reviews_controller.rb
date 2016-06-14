97cdf7e1abd2a462b9c4fe1193567bca9b0aae51
Kurt Schlueter <kurtbschlueter@gmail.com>
2016-06-14 11:51:21 -0500

    form data now saving to database

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,28 +21,60 @@ class ReviewsController < ApplicationController
 
   def create
 
-    puts '--------------create-----------'
-    puts params
-    puts '----------------------------------'
-    redirect_to '/'
-    # @article = Article.new(article_params)
-    # @article.user = current_user
-    # @article.save
-    # flash.notice = "Article '#{@article.title}' Created!"
-    # redirect_to article_path(@article)
-  end
+    empty_fields = []
 
-  def yes
+    params[:review_data].each do |key, value|
+      if value == ''
+        empty_fields << key
+      end
+    end
 
-    puts '--------------create-----------'
-    puts params
-    puts '----------------------------------'
-    redirect_to '/'
-    # @article = Article.new(article_params)
-    # @article.user = current_user
-    # @article.save
-    # flash.notice = "Article '#{@article.title}' Created!"
-    # redirect_to article_path(@article)
+    if empty_fields.length == 0
+      @user = current_user
+
+      @coach = Coach.find(params[:review_data][:coach_id].to_i)
+      @school = School.find(params[:review_data][:school_id].to_i)
+      @program = Program.find(params[:review_data][:program_id].to_i)
+
+
+      @review = Review.new(user: @user, coach: @coach, school: @school, program: @program,
+              hc_player_relationships: params[:review_data][:hc_player_relationships].to_i,
+              hc_player_development: params[:review_data][:hc_player_development].to_i,
+              hc_recruiting: params[:review_data][:hc_recruiting].to_i,
+              hc_pro_connections: params[:review_data][:hc_pro_connections].to_i,
+              hc_sport_knowledge: params[:review_data][:hc_sport_knowledge].to_i,
+              hc_would_play_with_again: true,
+              hc_comments: params[:review_data][:hc_comments],
+              f_main_arena: params[:review_data][:f_main_arena].to_i,
+              f_home_atmosphere: params[:review_data][:f_home_atmosphere].to_i,
+              f_weight_room: params[:review_data][:f_weight_room].to_i,
+              f_locker_room: params[:review_data][:f_locker_room].to_i,
+              f_training_facility: params[:review_data][:f_training_facility].to_i,
+              f_comments: params[:review_data][:f_comments],
+              l_program_tradition: params[:review_data][:l_program_tradition].to_i,
+              l_community_interest: params[:review_data][:l_community_interest].to_i,
+              l_weather: params[:review_data][:l_weather].to_i,
+              l_nightlife: params[:review_data][:l_nightlife].to_i,
+              l_comments: params[:review_data][:l_comments],
+              e_school_difficulty: params[:review_data][:e_school_difficulty].to_i,
+              e_academic_support: params[:review_data][:e_academic_support].to_i,
+              e_school_reputation: params[:review_data][:e_school_reputation].to_i,
+              e_graduated: true,
+              e_comments: params[:review_data][:e_comments],
+              conference_rival: params[:review_data][:conference_rival],
+              toughest_conference_place_to_play: params[:review_data][:toughest_conference_place_to_play])
+
+      if @review.save
+          render :json => {:empty_fields => ['save']}
+      else
+        render :json => {:empty_fields => ['no save','c']}
+      end
+    else
+
+        render :json => {:empty_fields => empty_fields}
+
+    end
+
   end
 
   def destroy
